80adbbc7a6f64da1b524160077ef51f3ba47a591
Gerasik <gerasik1992@gmail.com>
2020-09-02 01:04:51 +0300

    build page

---
+++ precache-manifest.3062278b1079c51d262c5f5eb1fac1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803eec1025133d13dbc9f96dfc0a16e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f202d3d6c35446dc0a49",
+    "url": "./static/css/main.c456fc3e.chunk.css"
+  },
+  {
+    "revision": "55b6d04e43c393cc5e7f",
+    "url": "./static/js/2.eb23500a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.eb23500a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f202d3d6c35446dc0a49",
+    "url": "./static/js/main.d61d6448.chunk.js"
+  },
+  {
+    "revision": "23d5b1f913e7ac55ef6f",
+    "url": "./static/js/runtime-main.f7490bbe.js"
+  }
+]);
